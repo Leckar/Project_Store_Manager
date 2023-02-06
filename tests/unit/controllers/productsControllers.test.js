22c7032389470afe5d0e358ea52c7c1af87f1f1c
Leckar <gusleckar@gmail.com>
2023-02-06 19:52:08 -0300

    req 20 parcially done

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -56,5 +56,78 @@ describe('The service layer should be able to manage all data successfully', fun
       expect(res.status).to.have.been.calledWith(200);
       expect(res.json).to.have.been.calledWith([{ id: 1, name: 'Martelo de Thor' }]);
     });
+    it('should return  error if has no query', async function () {
+      const req = { query: { q: null } };
+      const res = {};
+      sinon.stub(productsServices, 'listAll').resolves({
+        type: null, message: [
+          { id: 1, name: 'Martelo de Thor' },
+          { id: 2, name: 'Traje de encolhimento' },
+          { id: 3, name: 'Escudo do Capitão América' }
+        ]
+      });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.searchProducts(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith([
+        { id: 1, name: 'Martelo de Thor' },
+        { id: 2, name: 'Traje de encolhimento' },
+        { id: 3, name: 'Escudo do Capitão América' }
+      ]);
+    });
+    it('should return error if fails to load the db and has no query', async function () {
+      const req = { query: { q: null } };
+      const res = {};
+      sinon.stub(productsServices, 'listAll').resolves({ type: NOT_FOUND_STATUS, message: PRODUCT_NOT_FOUND });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.searchProducts(req, res);
+
+      expect(res.status).to.have.been.calledWith(NOT_FOUND_STATUS);
+      expect(res.json).to.have.been.calledWith(PRODUCT_NOT_FOUND);
+    });
+    it('should return error if fails to load the db and has query', async function () {
+      const req = { query: { q: 'Xablau' } };
+      const res = {};
+      sinon.stub(productsServices, 'listByName').resolves({ type: NOT_FOUND_STATUS, message: PRODUCT_NOT_FOUND });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.searchProducts(req, res);
+
+      expect(res.status).to.have.been.calledWith(NOT_FOUND_STATUS);
+      expect(res.json).to.have.been.calledWith(PRODUCT_NOT_FOUND);
+    });
+  });
+  describe('The listProductById service should resolve coherent requests', function () {
+    afterEach(sinon.restore);
+    it('should return all products in an object array with id and product name', async function () {
+      const req = { params: { id:1 }};
+      const res = {};
+      sinon.stub(productsServices, 'listById').resolves({
+        type: null, message: [{
+          id: 1,
+          name: "Martelo de Thor"
+        }]
+      });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.listProductById(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith([{ id: 1, name: 'Martelo de Thor' }]);
+    });
+    it('should return an error if the given id is invalid', async function () {
+      const req = { params: { id: 999 } };
+      const res = {};
+      sinon.stub(productsServices, 'listById').resolves({ type: NOT_FOUND_STATUS, message: PRODUCT_NOT_FOUND });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.listProductById(req, res);
+
+      expect(res.status).to.have.been.calledWith(NOT_FOUND_STATUS);
+      expect(res.json).to.have.been.calledWith({ message: PRODUCT_NOT_FOUND });
+    });
   });
 });
