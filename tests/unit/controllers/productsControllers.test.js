474185c246a0aea644b08cd98a124e77ea890ba3
Leckar <gusleckar@gmail.com>
2023-02-06 20:32:18 -0300

    req 20 parcially done

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -8,6 +8,7 @@ const { httpStatuses } = require('../../../src/utils/httpStatuses');
 const { productsList } = require('../mocks');
 const { NOT_FOUND_STATUS, UNPROCESSABLE_ENTITY } = httpStatuses;
 const PRODUCT_NOT_FOUND = 'Product not found';
+const NAME_TOO_SHORT = '"name" length must be at least 5 characters long';
 const { expect } = chai;
 
 chai.use(sinonChai);
@@ -152,13 +153,13 @@ describe('The service layer should be able to manage all data successfully', fun
         }
       };
       const res = {};
-      sinon.stub(productsServices, 'insertProduct').resolves({ type: UNPROCESSABLE_ENTITY, message: '"name" length must be at least 5 characters long' });
+      sinon.stub(productsServices, 'insertProduct').resolves({ type: UNPROCESSABLE_ENTITY, message: NAME_TOO_SHORT });
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
       await productsControllers.createProduct(req, res);
 
       expect(res.status).to.have.been.calledWith(UNPROCESSABLE_ENTITY);
-      expect(res.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
+      expect(res.json).to.have.been.calledWith({ message: NAME_TOO_SHORT });
     });
   });
   describe('The updateProduct service should resolve coherent requests', function () {
@@ -179,20 +180,69 @@ describe('The service layer should be able to manage all data successfully', fun
       expect(res.status).to.have.been.calledWith(200);
       expect(res.json).to.have.been.calledWith([{ id: 1, name: 'Elemento X' }]);
     });
-    // it('should return all products in an object array with id and product name', async function () {
-    //   const req = {
-    //     body: {
-    //       name: 'Ele'
-    //     }
-    //   };
-    //   const res = {};
-    //   sinon.stub(productsServices, 'insertProduct').resolves({ type: UNPROCESSABLE_ENTITY, message: '"name" length must be at least 5 characters long' });
-    //   res.status = sinon.stub().returns(res);
-    //   res.json = sinon.stub().returns();
-    //   await productsControllers.createProduct(req, res);
+    it('should return all products in an object array with id and product name', async function () {
+      const req = {
+        params: { id: 1 },
+        body: {
+          name: 'Ele'
+        }
+      };
+      const res = {};
+      sinon.stub(productsServices, 'updateProduct').resolves({ type: UNPROCESSABLE_ENTITY, message: NAME_TOO_SHORT });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.updateProduct(req, res);
 
-    //   expect(res.status).to.have.been.calledWith(UNPROCESSABLE_ENTITY);
-    //   expect(res.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
-    // });
+      expect(res.status).to.have.been.calledWith(UNPROCESSABLE_ENTITY);
+      expect(res.json).to.have.been.calledWith({ message: NAME_TOO_SHORT });
+    });
+    it('should return all products in an object array with id and product name', async function () {
+      const req = {
+        params: { id: 999 },
+        body: {
+          name: 'Elemento X'
+        }
+      };
+      const res = {};
+      sinon.stub(productsServices, 'updateProduct').resolves({ type: NOT_FOUND_STATUS, message: PRODUCT_NOT_FOUND });
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      await productsControllers.updateProduct(req, res);
+
+      expect(res.status).to.have.been.calledWith(NOT_FOUND_STATUS);
+      expect(res.json).to.have.been.calledWith({ message: PRODUCT_NOT_FOUND });
+    });
+  });
+  // describe('The updateProduct service should resolve coherent requests', function () {
+  //   afterEach(sinon.restore);
+  //   it('should return all products in an object array with id and product name', async function () {
+  //     const req = {
+  //       params: { id: 1 },
+  //     };
+  //     const res = {};
+  //     sinon.stub(productsServices, 'removeProduct').resolves({ type: null, message: '' });
+  //     res.status = sinon.stub().returns(res);
+  //     // res.json = sinon.stub().returns();
+  //     await productsControllers.deleteProduct(req, res);
+
+  //     expect(res.status).to.have.been.calledWith(204);
+  //     // expect(res.json).to.have.been.calledWith([{ id: 1, name: 'Elemento X' }]);
+  //   });
+  // });
+  describe('The updateProduct service should resolve coherent requests', function () {
+    afterEach(sinon.restore);
+    it('should return all products in an object array with id and product name', async function () {
+      const req = {
+        params: { id: 1 },
+      };
+      const res = {};
+      sinon.stub(productsServices, 'removeProduct').resolves({ type: null, message: '' });
+      res.status = sinon.stub().returns(res);
+      res.end = sinon.stub().returns();
+      await productsControllers.deleteProduct(req, res);
+
+      expect(res.status).to.have.been.calledWith(204);
+      expect(res.end).to.have.been.calledWith();
+    });
   });
 });
